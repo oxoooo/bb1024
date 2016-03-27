754323f9cc2e9b8014ded147c44224e401006ed1
mj <tywf91@gmail.com>
2016-03-27 21:40:33 +0800

    Initial Commit

---
+++ BB1024/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BB1024
+//
+//  Created by 林满佳 on 16/3/27.
+//  Copyright © 2016年 林满佳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
